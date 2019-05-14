b954ccf1555cf4c0758adab92e4c723340d37326
Sean Sundberg <seansund@us.ibm.com>
2019-05-13 22:51:09 -0500

    Uncommitted changes

--- src/bin/init.sh
+++ src/bin/init.sh
@@ -3,12 +3,12 @@
 helpFunction()
 {
    echo ""
-   echo "Usage: $0 BM_API_KEY REGION RESOURCE_GROUP CLUSTER_NAME"
+   echo "Usage: $0 IBMCL_API_KEY REGION RESOURCE_GROUP CLUSTER_NAME"
    exit 1 # Exit script after printing help
 }
 
 if [[ -n "$1" ]]; then
-   BM_API_KEY="$1"
+   IBMCL_API_KEY="$1"
 fi
 if [[ -n "$2" ]]; then
    REGION="$2"
@@ -21,19 +21,19 @@ if [[ -n "$4" ]]; then
 fi
 
 # Print helpFunction in case parameters are empty
-if [[ -z "${BM_API_KEY}" ]] || [[ -z "${REGION}" ]] || [[ -z "${RESOURCE_GROUP}" ]] || [[ -z "${CLUSTER_NAME}" ]]
+if [[ -z "${IBMCL_API_KEY}" ]] || [[ -z "${REGION}" ]] || [[ -z "${RESOURCE_GROUP}" ]] || [[ -z "${CLUSTER_NAME}" ]]
 then
    echo "Some or all of the parameters are empty";
    helpFunction
 fi
 
 # Login to IBM Cloud CLI
-ibmcloud login --apikey "${API_KEY}" -r "${REGION}" -g "${RESOURCE_GROUP}" > /dev/null 2>&1
+ibmcloud login --apikey "${IBMCL_API_KEY}" -r "${REGION}" -g "${RESOURCE_GROUP}" > /dev/null 2>&1
 
 # Use IBM Cloud CLI to get kubectl cluster config (placed in /root/.bluemix/plugins/container-service/clusters/{CLUSTER_NAME})
 ibmcloud ks cluster-config --cluster "${CLUSTER_NAME}" > /dev/null 2>&1
 
 # Copy Kube Config YML and Kube Server Cert to /root/.kube directory (where kubectl looks for it)
-mkdir -p /root/.kube > /dev/null 2>&1
-find /root/.bluemix/plugins/container-service/clusters -name "*.yml" -exec cp {} /root/.kube/config \; > /dev/null 2>&1
-find /root/.bluemix/plugins/container-service/clusters -name "*.pem" -exec cp {} /root/.kube \; > /dev/null 2>&1
+mkdir -p ~/.kube > /dev/null 2>&1
+find ~/.bluemix/plugins/container-service/clusters -name "*.yml" -exec cp {} ~/.kube/config \; > /dev/null 2>&1
+find ~/.bluemix/plugins/container-service/clusters -name "*.pem" -exec cp {} ~/.kube \; > /dev/null 2>&1
