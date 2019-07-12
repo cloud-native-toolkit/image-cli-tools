0cd7cc7ba7ebabd831c5283017fe2517738ac602
Sean Sundberg <seansund@us.ibm.com>
2019-07-12 09:36:40 -0500

    Updates copy-secret-to-namespace script to not fail if source secret does not exist

--- src/bin/copy-secret-to-namespace.sh
+++ src/bin/copy-secret-to-namespace.sh
@@ -50,6 +50,12 @@ if [[ -z "${KUBECONFIG}" ]]; then
   source ${TMP_DIR}/.kubeconfig
 fi
 
+kubectl get secret ${SECRET_NAME} --namespace ${FROM_NAMESPACE} 1> /dev/null 2> /dev/null
+if [[ $? -ne 0 ]]; then
+  echo "*** ${SECRET_NAME} could not be found in ${FROM_NAMESPACE} namespace"
+  exit 0
+fi
+
 if [[ $(kubectl get secrets -n "${TO_NAMESPACE}" -o jsonpath='{ range .items[*] }{ .metadata.name }{ "\n" }{ end }' | grep ${SECRET_NAME} | wc -l | xargs) -eq 0 ]]; then
     echo "*** Copying ${SECRET_NAME} from ${FROM_NAMESPACE} namespace to ${TO_NAMESPACE} namespace"
 
